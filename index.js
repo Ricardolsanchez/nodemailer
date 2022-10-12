aac9d5d07d63695a29e765f282ee0f04d6996df5
Ricardolsanchez <sanchezveraricardo@gmail.com>
2022-10-12 09:41:51 -0500

    first deploy

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import EmailSender from "./SendEmail.js";
+import mongoose from "mongoose";
 
 
 // mongodb+srv://ricksanchez:<password>@mern.tg7kcif.mongodb.net/test
@@ -10,10 +11,22 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 //DATABASE CONNECTION
 
+const user = 'ricksanchez';
+const password = 'Ricardo1496'; 
+const uri = `mongodb+srv://${user}:${password}@mern.tg7kcif.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.connect(uri,
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+.then(() => console.log('base de datos conectada'))
+.catch(e => console.log(e))
 // **** Send APi
 
 app.post("/send", async (req, res) => {
@@ -24,7 +37,7 @@ app.post("/send", async (req, res) => {
     } catch (error) {
         res.status(404).json({ msg: "Error X" });
     }
-}); 
+});
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(`http://localhost:${port}`);
